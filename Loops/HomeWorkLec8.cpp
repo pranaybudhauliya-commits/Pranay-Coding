dd148834624fbf834c1edfcb05801fc1190f7656
Pranay <pranaybudhauliya@gmail.com>
2026-07-27 06:27:21 +0530

    Ignore exe files

---
+++ Loops/HomeWorkLec8.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+// int x = 4,y = 0;
+// while(x>=0){
+//     x--;
+//     y++;
+//     if(x==y)
+//     continue;
+//     else cout<<x<<" "<<y<<endl;
+// }
+   
+
+// int x = 4, y = 0;
+// while(x>=0){
+//     if(x==y)
+//     break;
+//     else cout<<x<<" "<<y<<endl;
+//     x--;
+//     y++;
+// }
+
+
+int t = 10;
+while(t/=2)
+cout<<"Hello"<<endl;
+
+}
