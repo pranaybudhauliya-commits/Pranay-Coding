103d8251d299a8b1261e39b96cb7c42d6c9b4e0e
Pranay <pranaybudhauliya@gmail.com>
2026-08-18 20:55:15 +0530

    Add C++ practice programs

--- PatternPrinting/Homework.cpp
+++ PatternPrinting/Homework.cpp
@@ -30,7 +30,6 @@
 //     }
 // }
 
-
 // #include <iostream>
 // using namespace std;
 // int main()
@@ -47,7 +46,6 @@
 //         cout << endl;
 //     }
 // }
-
 
 // #include <iostream>
 // using namespace std;
@@ -66,8 +64,6 @@
 //     }
 // }
 
-
-
 // #include <iostream>
 // using namespace std;
 // int main()
@@ -85,21 +81,82 @@
 //     }
 // }
 
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cout << "Enter n :";
+//     cin >> n;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << i << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int m, n;
+//     cout << "Enter Rows and Columns : ";
+//     cin >> m >> n;
+//     for (int i = 1; i <= m; i++)
+//     {
+//         for (int j = 1; j <= n; j++)
+//         {
+//             if (i == 1 || i == m || j == 1 || j == n)
+//                 cout << "* ";
+//             else
+//                 cout << "  ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cout << "Enter n : ";
+//     cin >> n;
+//     int mid = (n / 2 + 1);
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= n; j++)
+//         {
+//             if (i == mid || j == mid)
+//                 cout << "* ";
+//             else
+//                 cout << "  ";
+//         }
+//         cout<<endl;
+//     }
+// }
 
 
 
+// Odd Number Triangle Method 2
 #include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cout << "Enter n :";
+    cout << "Enter n: ";
     cin >> n;
+    int a = 1;
+    // WAP to print first n odd numbers
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= i; j++)
         {
-            cout << i << " ";
+            cout << a << " ";
+            a += 2;
         }
         cout << endl;
     }
